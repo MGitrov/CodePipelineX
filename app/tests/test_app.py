fdea2533e69e40019769430d410ef15132c439a8
Maxim Petrov <maximpetrov2612@gmail.com>
2024-08-24 16:44:41 +0300

    Adjusted the test file to project's directory structure

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,4 +1,9 @@
+import sys
+import os
 import unittest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app import app
 
 class BasicTests(unittest.TestCase): # Defines a set of test cases for my application.
